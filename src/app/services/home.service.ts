ec6d0e32f75508f09d4f94f1b82bb3f4b8359f92
RajendraNeo <rajendra.neosoft@gmail.com>
2023-07-12 23:55:44 +0530

    all home page images path change

--- src/app/services/home.service.ts
+++ src/app/services/home.service.ts
@@ -10,27 +10,27 @@ export class HomeService {
 
   trending = [
     {
-      image: '/assets/images/png/chairs.png',
+      image: 'assets/images/png/chairs.png',
       desc: 'White Wooden Chair',
       price: '1200',
     },
     {
-      image: '/assets/images/png/sofa.png',
+      image: 'assets/images/png/sofa.png',
       desc: 'Modern Sofa Couch',
       price: '1200',
     },
     {
-      image: '/assets/images/png/support-chair.png',
+      image: 'assets/images/png/support-chair.png',
       desc: 'Armen Living Jaguar Chair',
       price: '1200',
     },
     {
-      image: '/assets/images/png/watch.png',
+      image: 'assets/images/png/watch.png',
       desc: 'Armen Living Jaguar Chair',
       price: '1200',
     },
     {
-      image: '/assets/images/png/lamb.png',
+      image: 'assets/images/png/lamb.png',
       desc: 'Globe Desk Lamp',
       price: '1200',
     },
@@ -38,25 +38,25 @@ export class HomeService {
   
   discount=[
     {
-      image: '/assets/images/png/bedding-sets.png',
+      image: 'assets/images/png/bedding-sets.png',
       heading: 'Bedding Sets',
       desc: 'Floral Tales Collection',
       discount: '20%',
     },
     {
-      image: '/assets/images/png/chairs-sets.png',
+      image: 'assets/images/png/chairs-sets.png',
       heading: 'Soleil Dining Chair',
       desc: 'lightweight chair',
       discount: '20%',
     },
     {
-      image: '/assets/images/png/living-sets.png',
+      image: 'assets/images/png/living-sets.png',
       heading: 'Living Sets',
       desc: 'Easy-to-match shade of beige.',
       discount: '20%',
     },
     {
-      image: '/assets/images/png/outdoor-sets.png',
+      image: 'assets/images/png/outdoor-sets.png',
       heading: 'Outdoor Sets',
       desc: 'Simplest design solutions.',
       discount: '20%',
@@ -65,17 +65,17 @@ export class HomeService {
 
   budget = [
     {
-      image: '/assets/images/png/sofas-under-1.png',
+      image: 'assets/images/png/sofas-under-1.png',
       heading: 'Sofas under',
       price: '1200',
     },
     {
-      image: '/assets/images/png/sofas-under-2.png',
+      image: 'assets/images/png/sofas-under-2.png',
       heading: 'Sofas under',
       price: '1200',
     },
     {
-      image: '/assets/images/png/sofas-under-3.png',
+      image: 'assets/images/png/sofas-under-3.png',
       heading: 'Sofas under',
       price: '1200',
     },
@@ -83,42 +83,42 @@ export class HomeService {
 
   bestSeller = [
     {
-      image: '/assets/images/png/best-seller-1.png',
+      image: 'assets/images/png/best-seller-1.png',
       heading: 'White Wooden Chair',
       oldPrice: '8000',
       newPrice: '6000',
       star:4.2
     },
     {
-      image: '/assets/images/png/best-seller-2.png',
+      image: 'assets/images/png/best-seller-2.png',
       heading: 'White Wooden Chair',
       oldPrice: '8000',
       newPrice: '6000',
       star:3.8
     },
     {
-      image: '/assets/images/png/best-seller-3.png',
+      image: 'assets/images/png/best-seller-3.png',
       heading: 'White Wooden Chair',
       oldPrice: '8000',
       newPrice: '6000',
       star:4.6
     },
     {
-      image: '/assets/images/png/best-seller-4.png',
+      image: 'assets/images/png/best-seller-4.png',
       heading: 'White Wooden Chair',
       oldPrice: '8000',
       newPrice: '6000',
       star:3.5
     },
     {
-      image: '/assets/images/png/best-seller-5.png',
+      image: 'assets/images/png/best-seller-5.png',
       heading: 'White Wooden Chair',
       oldPrice: '8000',
       newPrice: '6000',
       star:4.5
     },
     {
-      image: '/assets/images/png/best-seller-6.png',
+      image: 'assets/images/png/best-seller-6.png',
       heading: 'White Wooden Chair',
       oldPrice: '8000',
       newPrice: '6000',
@@ -128,19 +128,19 @@ export class HomeService {
 
   blog = [
     {
-      image: '/assets/images/png/our-blog-1.png',
+      image: 'assets/images/png/our-blog-1.png',
       subHeading: 'Bedroom CHAIR',
       heading: 'Perfect Bedroom Corner Chair',
       desc: 'An accent chair is a great focal point, adds contrast, and complements the room’s decor.',
     },
     {
-      image: '/assets/images/png/our-blog-2.png',
+      image: 'assets/images/png/our-blog-2.png',
       subHeading: 'FURNITURE',
       heading: 'A Beautiful Sunday Morning',
       desc: 'An accent chair is a great focal point, adds contrast, and complements the room’s decor.',
     },
     {
-      image: '/assets/images/png/our-blog-3.png',
+      image: 'assets/images/png/our-blog-3.png',
       subHeading: 'DINING CHAIR',
       heading: 'Maximalism Design Style',
       desc: 'A maximalist space is interesting to look at. Contrast and color are everything.',
